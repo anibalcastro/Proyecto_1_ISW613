d54e93f03b24537a2e8d48edce311b02385e9907
anibalcastro <anibalcastro1515@gmail.com>
2022-03-02 22:51:09 -0600

    css

---
+++ Administrador/categorias.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Categorias</title>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <?php echo "<link rel=stylesheet href=categorias.css>"; ?>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+
+<body>
+  <header id="encabezado">
+    <nav id="navMenu" class="navbar navbar-light bg-light">
+      <a id="encLogo" class="navbar-brand" href="#">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Telefe_Noticias_logo_2_%282018%29.png"
+          id="logo_empresa" alt="icon" srcset="logo icon">
+      </a>
+      <div id="menubarra" class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+        <div class="btn-group mr-2" role="group" aria-label="Second group">
+          <button type="button" class="btn btn-dark">Administrador</button>
+          <button type="button" class="btn btn-dark">Categories</button>
+          <button type="button" class="btn btn-primary">Log out</button>
+        </div>
+      </div>
+    </nav>
+  </header>
+</body>
+
+</html>
