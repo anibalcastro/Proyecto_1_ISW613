3e164e8fb5a478e2aba479269a2105c53a00d01b
anibalcastro <anibalcastro1515@gmail.com>
2022-03-04 12:18:31 -0600

    update

--- Registro/registro.php
+++ Registro/registro.php
@@ -16,7 +16,7 @@
 
 <body>
   <header id="encabezado">
-    <nav class="navbar navbar-light bg-light">
+    <nav id="nav" class="navbar navbar-light bg-light">
       <a class="navbar-brand" href="#">
       <img src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Telefe_Noticias_logo_2_%282018%29.png"
           id="logo_empresa" alt="icon" srcset="logo icon">
