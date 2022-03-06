7c681581da501796bb015397d4847dc61e3d4f61
anibalcastro <anibalcastro1515@gmail.com>
2022-03-06 12:55:49 -0600

    update sql

--- Registro/lo_registro.php
+++ Registro/lo_registro.php
@@ -15,7 +15,7 @@
         $connection = mysqli_connect('127.0.0.1','root','','mynews');
 
         //consulta
-        $sqlInsert = "INSERT INTO `users`( `email`, `password`, `first_name`, `last_name`, `adress`, `adress2`, `country`, `city`, `zip`, `phone`, `role_id`) VALUES ('$email','$password','$name','$last_Name','$adress','$adress2','$country','$city','$city','$phone', 2)";
+        $sqlInsert = "INSERT INTO `users`( `email`, `password`, `first_name`, `last_name`, `adress`, `adress2`, `country`, `city`, `zip`, `phone`, `role_id`) VALUES ('$email','$password','$name','$last_Name','$adress','$adress2','$country','$city','$zip','$phone', 2)";
 
         //ejecutamos consulta
         mysqli_query($connection, $sqlInsert);
