2b5d88a9be7bd58eaf8a82c7126446e33d34362a
anibalcastro <anibalcastro1515@gmail.com>
2022-03-06 10:43:51 -0600

    update

--- Registro/registro.php
+++ Registro/registro.php
@@ -17,7 +17,7 @@
 <body>
   <header id="encabezado">
     <nav id="nav" class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="http://utnweb.com/web2/Proyecto_1_ISW613/index.php">
       <img src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Telefe_Noticias_logo_2_%282018%29.png"
           id="logo_empresa" alt="icon" srcset="logo icon">
       </a>
--- index.php
+++ index.php
@@ -17,11 +17,10 @@
 <body>
 <header id="encabezado">
     <nav id="nav" class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="http://utnweb.com/web2/Proyecto_1_ISW613/index.php">
       <img src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Telefe_Noticias_logo_2_%282018%29.png"
           id="logo_empresa" alt="icon" srcset="logo icon">
       </a>
-      <button id="btn_login" class="btn btn-dark my-2 my-sm-0" type="submit">Log in</button>
     </nav>
   </header>
 
