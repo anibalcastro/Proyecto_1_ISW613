c2a36dc7b528a6c36ab60778c1ab7e6bff73d80f
anibalcastro <anibalcastro1515@gmail.com>
2022-03-08 23:56:57 -0600

    xml completed

---
+++ Usuario/xml.php
@@ -0,0 +1,39 @@
+<?php 
+
+$url = "http://feeds.feedburner.com/crhoy/wSjk?format=xml";
+
+$invalidurl = false;
+if(@simplexml_load_file($url)){
+ $feeds = simplexml_load_file($url);
+}else{
+ $invalidurl = true;
+ echo "<h2>Invalid RSS feed URL.</h2>";
+}
+
+
+$i=0;
+if(!empty($feeds))
+{
+
+ $site = $feeds->channel->title;
+ $sitelink = $feeds->channel->link;
+
+ foreach ($feeds->channel->item as $item) {
+
+  $title = $item->title;
+  $link = $item->link;
+  $description = $item->description;
+  $pubDate = $item->pubDate;
+  $categoria = $item->category;
+
+
+  echo "Titulo: ". $title.PHP_EOL;
+  echo "Link:".$link.PHP_EOL;
+  echo "Descripcion:".$description.PHP_EOL;
+  echo "Publicacion:".$pubDate.PHP_EOL;
+  echo "Categoria:".$categoria.PHP_EOL;
+  echo "".PHP_EOL;
+  echo "".PHP_EOL;
+  echo "".PHP_EOL;
+ }
+}
