008951235afab45c40aea78260679239d28d5cdb
anibalcastro <anibalcastro1515@gmail.com>
2022-03-15 00:57:18 -0600

    update

--- Usuario/funcionesUsuario.php
+++ Usuario/funcionesUsuario.php
@@ -79,7 +79,13 @@ function getIdUsuario()
  */
 function getNewByCategories($categories, $id)
 {
-    
+    $connection = conexion();
+
+    $sql = "SELECT title, short_description, perman_link, fecha, news_source.name, categories.name FROM `news` INNER JOIN news_source ON news.news_source_id = news_source.id INNER JOIN categories ON news.category_id = categories.id AND news.user_id = $id AND news.category_id = $categories ORDER BY fecha DESC;";
+
+    $result = mysqli_query($connection, $sql);
+    mysqli_close($connection);
+
 }
 
 /**
@@ -94,6 +100,7 @@ function getAllNewByIdUser($id)
     echo $sql;
 
     $result = mysqli_query($connection, $sql);
+    mysqli_close($connection);
     var_dump($result);
 }
 
