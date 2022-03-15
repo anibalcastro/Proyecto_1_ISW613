20a024f1ef519eae3754d0bab8059a4eb2bb9826
anibalcastro <anibalcastro1515@gmail.com>
2022-03-15 00:58:19 -0600

    update

--- Usuario/funcionesUsuario.php
+++ Usuario/funcionesUsuario.php
@@ -77,14 +77,17 @@ function getIdUsuario()
 /**
  * Get news by Categories
  */
-function getNewByCategories($categories, $id)
+function getNewByCategories($idCategory, $id)
 {
     $connection = conexion();
 
-    $sql = "SELECT title, short_description, perman_link, fecha, news_source.name, categories.name FROM `news` INNER JOIN news_source ON news.news_source_id = news_source.id INNER JOIN categories ON news.category_id = categories.id AND news.user_id = $id AND news.category_id = $categories ORDER BY fecha DESC;";
+    $sql = "SELECT title, short_description, perman_link, fecha, news_source.name, categories.name FROM `news` INNER JOIN news_source ON news.news_source_id = news_source.id INNER JOIN categories ON news.category_id = categories.id AND news.user_id = $id AND news.category_id = $idCategory ORDER BY fecha DESC;";
 
     $result = mysqli_query($connection, $sql);
     mysqli_close($connection);
+
+    echo $sql;
+    var_dump($result);
 
 }
 
